f22a600499504df352420d0ecfdc6e02e330d849
maksfourlife <maksfourlife@gmail.com>
2026-01-16 13:18:42 +0100

    solution for #131 palindrome partitioning

--- rust/examples/131_palindrome_partitioning.rs
+++ rust/examples/131_palindrome_partitioning.rs
@@ -2,78 +2,78 @@
 
 #![allow(clippy::ptr_arg)]
 
-// use std::{collections::HashSet, ops::Range};
+use std::{collections::HashSet, ops::Range};
 
-// fn partition(s: &str) -> Vec<Vec<String>> {
-//     let mut output = vec![];
-//     let orig = s.as_bytes();
-//     f(orig, 0..s.len(), vec![], &mut HashSet::new(), &mut output);
-//     output
-//         .into_iter()
-//         .map(|x| {
-//             x.into_iter()
-//                 .map(|r| unsafe { std::str::from_utf8_unchecked(&orig[r]) }.to_string())
-//                 .collect::<Vec<_>>()
-//         })
-//         .collect()
-// }
+fn partition(s: &str) -> Vec<Vec<String>> {
+    let palindrome_set = palindrome_set(s);
 
-// fn f(
-//     orig: &[u8],
-//     range: Range<usize>,
-//     curr: Vec<Range<usize>>,
-//     is_palindrome: &mut HashSet<Range<usize>>,
-//     output: &mut Vec<Vec<Range<usize>>>,
-// ) {
-//     let it = range.start + 1..range.end;
+    let mut palindromes: Vec<_> = palindrome_set.into_iter().collect();
+    palindromes.sort_unstable_by_key(|x| x.start);
 
-//     if it.is_empty() {
-//         dbg!(&curr);
-//     } else {
-//         for i in it {
-//             let left = range.start..i;
-//             let right = i..range.end;
-//             f(
-//                 orig,
-//                 right.clone(),
-//                 [curr.clone(), vec![left.clone()]].concat(),
-//                 is_palindrome,
-//                 output,
-//             );
-//             f(
-//                 orig,
-//                 left,
-//                 [curr.clone(), vec![right]].concat(),
-//                 is_palindrome,
-//                 output,
-//             );
-//         }
-//     }
-// }
+    let mut out = vec![];
+    f(&palindromes, vec![], &mut out, s.len());
 
-use std::ops::Range;
+    let bytes = s.as_bytes();
 
-fn f(orig: &[u8], range: Range<usize>) -> Vec<Vec<Vec<u8>>> {
-    dbg!(&range);
-    let it = range.start + 1..range.end;
-    if it.is_empty() {
-        vec![vec![vec![orig[range.start]]]]
-    } else {
-        it.flat_map(|i| {
-            let left = f(orig, range.start..i);
-            let right = f(orig, i..range.end);
-            left.into_iter().flat_map(move |prefix| {
-                right
-                    .clone()
-                    .into_iter()
-                    .map(move |postfix| [prefix.clone(), postfix].concat())
-            })
+    out.into_iter()
+        .map(|ranges| {
+            ranges
+                .into_iter()
+                .map(|range| unsafe { std::str::from_utf8_unchecked(&bytes[range]) }.to_string())
+                .collect::<Vec<_>>()
         })
         .collect()
+}
+
+fn f(
+    ranges: &[Range<usize>],
+    curr: Vec<Range<usize>>,
+    out: &mut Vec<Vec<Range<usize>>>,
+    target_len: usize,
+) {
+    ranges
+        .iter()
+        .enumerate()
+        .filter(|(_, new_range)| new_range.start == curr.last().map(|x| x.end).unwrap_or(0))
+        .for_each(|(i, new_range)| {
+            let mut new_curr = curr.clone();
+            new_curr.push(new_range.clone());
+            f(&ranges[i + 1..], new_curr, out, target_len);
+        });
+    if let Some(last) = curr.last()
+        && last.end == target_len
+    {
+        out.push(curr);
     }
 }
 
+fn palindrome_set(s: &str) -> HashSet<Range<usize>> {
+    let s = s.as_bytes();
+
+    let mut is_palindrome = HashSet::with_capacity(s.len() * (1 + s.len()) / 2);
+
+    for i in 0..s.len() {
+        is_palindrome.insert(i..i + 1);
+    }
+
+    for k in 1..=s.len() {
+        for i in 0..s.len() - k + 1 {
+            let curr = i..i + k;
+            let prev = curr.start + 1..curr.end - 1;
+
+            if (prev.is_empty() || is_palindrome.contains(&prev))
+                && s[curr.start] == s[curr.end - 1]
+            {
+                is_palindrome.insert(curr);
+            }
+        }
+    }
+
+    is_palindrome
+}
+
 fn main() {
-    let s = "aabcb";
-    dbg!(f(s.as_bytes(), 0..s.len()));
+    assert_eq!(partition("a"), [["a"]]);
+    dbg!(partition("aab"));
+    dbg!(partition("aabcb"));
 }
